5a1f41493d59b9ddc8eef5ba83ab6134bc7bf4d8
yakirevich <yakirevich@cs.huji.ac.il>
2025-03-09 23:09:48 +0200

    github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/ai_app',
+  assetPrefix: '/ai_app/',
 };
 
 export default nextConfig;
